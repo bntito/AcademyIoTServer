424f8c5422b770d67208fe5d5fc42275553ad94f
Bruno Tito <bruno.nicolas.tito@gmail.com>
2024-05-23 12:35:06 -0300

    change cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ require('./config/mysql/configDB.js');
 
 app.use(morgan('dev'));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://tubular-klepon-496e56.netlify.app/',
   credentials: true
 }));
 app.use(express.json());
